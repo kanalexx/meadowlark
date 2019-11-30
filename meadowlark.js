37825fd03d2ab211b64e818ba2c2337a1fe60908
kanal <kanalexx@gmail.com>
2019-11-30 12:38:53 +0300

    Страничное тестирование

--- meadowlark.js
+++ meadowlark.js
@@ -14,13 +14,23 @@ app.use(express.static(__dirname + '/public'));
 // установка порта для сервера
 app.set('port', process.env.PORT || 3000);
 
+// маршрут для теста
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' 
+        && req.query.test === '1';
+    next();
+});
+
 // маршруты
 app.get('/', function(req, res){
     res.render('home');
 });
 
 app.get('/about', function(req, res){
-    res.render('about', {fortune: fortune.getFortune()});
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
 // пользовательская страница 404
